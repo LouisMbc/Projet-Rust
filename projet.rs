8cdaca77bd4dc49dee98636de3a85205ba904fd5
LouisMbc <louis.malbec@ecole-hexagone.com>
2025-12-06 14:02:05 +0100

    Emprunt livre

--- projet.rs
+++ projet.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+//structure livre
 #[derive(Clone)]
 struct Livre {
     titre: String,
@@ -14,7 +15,60 @@ fn lire_ligne() -> String {
     entree.trim().to_string()
 }
 
+//Ajouter livres
+fn ajouter_livre(biblio: &mut Vec<Livre>) {
+    println!("Titre du livre :");
+    let titre = lire_ligne();
+
+    for livre in biblio.iter() {
+        if livre.titre == titre {
+            println!("Un livre avec ce titre existe déjà");
+            return;
+        }
+    }
+
+    println!("Auteur :");
+    let auteur = lire_ligne();
+
+    println!("Année :");
+    let annee_str = lire_ligne();
+    let annee = annee_str.parse::<u32>().unwrap_or(0);
+
+    let nouveau = Livre {
+        titre,
+        auteur,
+        annee,
+        disponible: true,
+    };
+
+    biblio.push(nouveau);
+    println!("Livre ajouté !");
+}
+
+// emprunter livre
+fn emprunter_livre(biblio: &mut Vec<Livre>) {
+    println!("Titre du livre à emprunter :");
+    let titre = lire_ligne();
+
+    for livre in biblio.iter_mut() {
+        if livre.titre == titre {
+            if livre.disponible {
+                livre.disponible = false;
+                println!("Livre emprunté");
+            } else {
+                println!("Ce livre est déjà emprunté");
+            }
+            return;
+        }
+    }
+
+    println!("Livre introuvable");
+}
+
 fn main() {
-    let mut _biblio: Vec<Livre> = Vec::new();
-    println!("livre");
+    let mut biblio: Vec<Livre> = Vec::new();
+    
+    println!("ajout et  emprunt");
+    ajouter_livre(&mut biblio);
+    emprunter_livre(&mut biblio);
 }
